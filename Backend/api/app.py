65142aa7305584654676b1aaa024c0e85647a503
Rohit-43 <rohitsainisameer@gmail.com>
2024-11-27 00:10:51 +0530

    Backend Deployment

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -3,7 +3,9 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_cors import CORS
+from scripts.generate_auth import generate_auth_file
 
+generate_auth_file()
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
